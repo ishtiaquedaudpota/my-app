06e34d2db7d14acbc82bd73135542dc202bc16cb
user <user@localhost>
2018-11-16 10:14:19 +0800

    Aadfadsf

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,22 @@
 package com.mycompany.app;
+import org.apache.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    final static Logger logger = Logger.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        (new App()).log("Hello World!");        
+    }
+    private void log(String msg){
+        if(logger.isDebugEnabled()) {
+            logger.debug("This is debug : " + msg);
+        }
+        if(logger.isInfoEnabled()) {
+            logger.info("This is info : " + msg);
+        }
+        logger.warn("This is warn : " + msg);
+        logger.error("This is error : " + msg);
+        logger.fatal("This is fatal : " + msg);
     }
 }
+
